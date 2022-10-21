495d8c27438f12c7d09f79e021e14b07d016932e
llss1989 <argenkasymov2@gmail.com>
2022-10-21 20:12:33 +0600

    corrections were added as per comments

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,20 +1,10 @@
 FactoryBot.define do
-  sequence :string2, aliases: [:name, :description] do |n|
-    "string#{n}"
-  end
   factory :task do
     name
     description
     author_id { 1 }
     assignee_id { 1 }
-    state { ""}
-    expired_at { "#{time_rand}" }
+    state { '' }
+    expired_at
   end
 end
-
-
-def time_rand from = 0.0, to = Time.now
-  Time.at(from + rand * (to.to_f - from.to_f)).to_s.split('')[0, 10].join()
-end
-
-
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,33 +1,22 @@
-
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
-    "string#{n}"
-  end
-  sequence :email do |n| 
-    "email#{n}@example.com"
-  end
-  sequence :avatar do |n|
-    "~/app/assets/images/#{n}"
-  end
-  
   factory :user do
     first_name
     last_name
     password
     email
     avatar
-    type { "" }
+    type { '' }
 
     factory :developer do
-      type { "Developer" }
+      type { 'Developer' }
     end
 
     factory :manager do
-      type { "Manager" }
+      type { 'Manager' }
     end
 
     factory :admin do
-      type { "Admin" }
+      type { 'Admin' }
     end
   end
 end
