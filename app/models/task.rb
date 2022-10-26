1c27d24bcfb00736f2487d5d11cb81c2280b789a
llss1989 <argenkasymov2@gmail.com>
2022-10-26 13:27:16 +0600

    added step about state-machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,41 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+  state_machine initial: :new_task do
+    state :new_task
+    state :in_development
+    state :archived
+    state :in_qa
+    state :in_code_review
+    state :ready_for_release
+    state :released
+
+    event :start do
+      transition new_task: :in_development
+    end
+
+    event :test_code do
+      transition in_development: :in_qa
+    end
+
+    event :review do
+      transition in_qa: :in_code_review
+    end
+    event :back_to_development do
+      transition [:in_qa, :in_code_review] => :in_development
+    end
+
+    event :ready_for_release_or_not do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :release do
+      transition ready_for_release: :released
+    end
+
+    event :archive do
+      transition [:new_task, :ready_for_release, :released] => :archived
+    end
+  end
+  
 end
