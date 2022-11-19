0aa0fe048ddf79091767c3855f831d165804e59d
llss1989 <argenkasymov2@gmail.com>
2022-11-19 22:54:00 +0600

    corrected path import

--- app/javascript/utils/fetchHelper.js
+++ app/javascript/utils/fetchHelper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import qs from 'qs';
 
-import { camelize, decamelize } from './keysConverter.js';
+import { camelize, decamelize } from './keysConverter';
 
 function authenticityToken() {
   const token = document.querySelector('meta[name="csrf-token"]');
