6417780003e3ef1f0b009e3e05cbf808eb58de65
llss1989 <argenkasymov2@gmail.com>
2022-11-28 20:32:50 +0600

    delete unused path

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import WebpackerReact from 'webpacker-react';
-// import TaskBoard from 'components/TaskBoard';
 import App from '../App';
 
 Rails.start();
