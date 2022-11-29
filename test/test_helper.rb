43cdf09ca21233ebff159900bf190a098bf49967
llss1989 <argenkasymov2@gmail.com>
2022-11-29 18:57:41 +0600

    added Icov.info

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,6 @@ require 'simplecov'
 SimpleCov.start 'rails' do
   if ENV['CI']
     require 'simplecov'
-    
     SimpleCov::Formatter::LcovFormatter.config do |c|
       c.report_with_single_file = true
       c.single_report_path = 'coverage/lcov.info'
